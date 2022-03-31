2f8d055832c3c7bc2cce714a06b03d9c2b67ea68
AMiKaH <ameer.kubba@yahoo.com>
2022-03-31 13:30:09 -0700

    asd

--- backend/pytest/pytest.py
+++ backend/pytest/pytest.py
@@ -84,3 +84,15 @@ def random5(x, y) :
         return x*y
     
     return x+y
+
+def random6(x, y) :
+    if x == 0:
+        return y
+    
+    if y == 0:
+        return x
+
+    if x + y == 2:
+        return x*y
+    
+    return x+y
